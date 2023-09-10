f3138b92da7bf56b4eea431f391275037aa0a9c3
maximilian fleck <fleck@itt.uni-stuttgart.de>
2023-09-11 00:07:23 +0200

    added battery check :)

--- code/camera_main.py
+++ code/camera_main.py
@@ -7,7 +7,6 @@ import neopixel
 import board
 import picamera
 import picamera.array
-#import matplotlib.image
 import cv2
 import numpy as np
 from scipy import ndimage
@@ -17,29 +16,19 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 class usb_mount():
-    """
-    usb_mount class
     
-    the whole software of the camera is build around the availability
-    of an usb storage device as if pictures can be taken and safed depends on this.
-    
-    
-    
-    """
-    
-    def __init__(self, mount_folder, filter_fun, pixel_pin, 
+    def __init__(self, mount_folder, filter_fun, pixel_pin,charge_pin, 
                  color=(255, 0, 0) ):
-        """
-        Initializing the usb_mount class
         
-        - theres a lot of hard coded stuff right now
-        - ... but it works
-        
-        """
         self.camera = picamera.PiCamera()
         
-        self.pixel = neopixel.NeoPixel(pixel_pin, 1,)
+        self.charge_pin = charge_pin
+        GPIO.setup(self.charge_pin, GPIO.IN)
+        
+        #ORDER = neopixel.RGB
+        self.pixel = neopixel.NeoPixel(pixel_pin, 1,) # pixel_order=ORDER)
         self.pixel[0] = color
+        print(self.pixel[0])
 
         self.usb_status = False
         self.umount()
@@ -58,16 +47,19 @@ class usb_mount():
         # part of color setter 
         self.color_nousb     = (255,0,0)
         self.color_usb       = (0,255,0)
-        self.color_camoff    = (0,0,255)
+        self.color_camoff    = (0,155,255)
         self.color_children  = (32,200,150)
+        self.color_battery   = (255, 200, 20)
 
         #no = self.get_no_children()
         #print("ini no of children:",no)
         self.active_children = False
         self.active_camera = False
+        self.battery_low = False
 
         #
         self.res = (1312,976) # ( vertical, horizontal )
+        #self.rres = int( (round(self.res/16)+1)*16 )
         self.res_shift_horizontal = 40
         self.res_shift_vertical   = 240
         self.res_pic_xy           = 800
@@ -84,14 +76,14 @@ class usb_mount():
         self.rotate_img = 267.5
         self.rotate_img = 272.5
         
-        self.trigger_count = 2
+        self.trigger_count = 1
         self.no = -1
         return
 
 
     def mount(self):
         """
-        mounts usb device
+        mounts usb
         """        
         devices = glob.glob("/dev/sd*")
         for device in devices:
@@ -120,10 +112,7 @@ class usb_mount():
 
     def umount(self):
         """
-        unmounts usb device
-        
-        (call this fun when usb remove is detected)
-        
+        unmounts usb
         """
         devices = glob.glob("/dev/sd*")
         for device in devices:
@@ -166,28 +155,19 @@ class usb_mount():
     
     def knob_function_to_be_called( self, gpio_pin_no ):
         """
-        this function is called when the knob is pressed:
+        if knob is pressed:
+        - check for devices
+        - if USB: take photo and call postproc
+        - else: blink, do nothing
         
-        there are 4 things that need to be true for the cam to be triggered:
-        
-        - trigger_count needs to be correct. 
-            - for some knobs the function is triggered twice
-            - high bouncetimes are no solution
-            - ... we need to count the triggers
-            - usually every 2nd call is a true call
-        - the camera needs to be active (active_action == True)
-        - the camera needs not to be in use (active_camera == False)
-        - a USB storage device needs to be plugged in and mounted
-        
-        when the photo was taken a subprocess is started to do the postprocessing
-        i.e. apply filters and save the result to the storage device
-            
-        gpio_pin_no is a dummy and not needed because the pin used is predefined
+        TO DO
+        - blink does work ?
+        - knob ? ... bouncetime high enough? :)
         
         """
         print("\nKNOB PRESSED",self.no)
         self.trigger_count += 1
-        if self.trigger_count % self.trigger_count == 0:
+        if self.trigger_count % 2 == 0:
             if self.active_action and not self.active_camera:
                 print("KNOB activated\n")      
                 if self.usb_status: #and self.get_no_children() < 10:
@@ -214,40 +194,30 @@ class usb_mount():
         return
     
     def stop_camera(self):
-        """
-        calling this fun stops the camera
-        """
         print("stop camera")
         self.camera.stop_preview()
         return
     
     def start_camera(self):
-        """
-        calling this fun starts the camera
-        """        
         print("start camera")
         self.camera.start_preview()
-        time.sleep(1.)
+        time.sleep(1)
         print("camera ready")
         return
     
     def filter_wrapper(self,img,no):
         """
-        - rotates picture
-        - trims picture according to camera viewfinder
-        - wraps filter fun :)
-        - saves results
-        - syncs storage device
-        
-        img: np.array with rgb values for each pixel
-        
-        no: int for unique savename
-        
+        wraps filter fun :)
         """
         print("postproc called",no)
+        #img = np.rot90(img,3)
         img = ndimage.rotate(img, self.rotate_img )
+        #print( "img shape",img.shape )
+        #img = cv2.resize(img,(self.rres_plus,self.rres_plus))
+        print( "img shape",img.shape )
         img = img[ self.res_shift_vertical:self.res_pic_xy+self.res_shift_vertical, 
                   self.res_shift_horizontal:self.res_pic_xy+self.res_shift_horizontal ,:]
+        print( "img shape",img.shape )
         
         save_path_raw    = self.save_folder_raw+self.picname+str(no)+".png"
         save_path_filter = self.save_folder_filter+self.picname+str(no)+".png"
@@ -270,6 +240,10 @@ class usb_mount():
         """
         children = active_children()
         no = len(children)
+        #if no == 0:
+        #    self.active_children = False
+        #else:
+        #    self.active_children = True
         return no
     
     def check_children(self):
@@ -278,6 +252,7 @@ class usb_mount():
         """        
         children = active_children()
         no = len(children)
+        #print( no, self.active_children )
         if no == 0 and self.active_children:        
             self.active_children = False
             return 1
@@ -287,30 +262,24 @@ class usb_mount():
         return 0
 
     def color_setter(self):
-        """
-        color setter fun for rgb pixel
-        
-        adjusts color of the status LED based on flags
-        
-        RULE for clear code:
-        
-        ONLY this fun and the fun triggered when the knob is pressed
-        should manipulate the color of the status LED !!
-        
-        """
         if self.usb_status:
-            if not self.active_action:
-                print("not active", self.color_camoff)
-                self.pixel[0]      = self.color_camoff
-                self.current_color = self.color_camoff
-            elif self.active_children:
-                print("children", self.color_children)
-                self.pixel[0]      = self.color_children
-                self.current_color = self.color_children                   
+            if self.battery_low:
+                print("battery low", self.color_battery)
+                self.pixel[0]      = self.color_battery
+                self.current_color = self.color_battery               
             else:
-                print("usb", self.color_usb)
-                self.pixel[0] = self.color_usb
-                self.current_color = self.color_usb
+                if not self.active_action:
+                    print("not active", self.color_camoff)
+                    self.pixel[0]      = self.color_camoff
+                    self.current_color = self.color_camoff
+                elif self.active_children:
+                    print("children", self.color_children)
+                    self.pixel[0]      = self.color_children
+                    self.current_color = self.color_children                   
+                else:
+                    print("usb", self.color_usb)
+                    self.pixel[0] = self.color_usb
+                    self.current_color = self.color_usb
         else:
             print("no usb")
             self.pixel[0] = self.color_nousb
@@ -319,13 +288,6 @@ class usb_mount():
         return
     
     def blink(self, zzz, times):
-        """
-        makes the LED flash at a certain frequency
-        
-        might be unused...
-        ...dont use !
-        
-        """
         for _ in range(times):
             self.pixel[0] = (0,0,0)
             time.sleep( zzz )
@@ -335,44 +297,40 @@ class usb_mount():
         return
     
     def time_event(self):
-        """
-        call this fun when something happens
-        
-        based on the last time event the cam will go to sleep
-        
-        """
         self.active_action = True
         self.active_last_seconds = time.time()
         return
     
-    def check_active_time(self, time_to_sleep=60):
-        """
-        checks when the last event happened
-        
-        if it is too far back the camera is sent to sleep
-        
-        time_to_sleep: 
-        - time in seconds
-        - after this time the cam goes to sleep
-        
-        """
+    def check_active_time(self, diff=30):
         time_diff = time.time() - self.active_last_seconds
-        if time_diff > time_to_sleep and self.active_action:
+        if time_diff > diff and self.active_action:
             self.active_action = False
             self.stop_camera()
             return 1
         return 0
-        
+
+    def check_battery(self):
+        bat = GPIO.input(self.charge_pin)
+        if not self.battery_low and not bat:
+            self.battery_low = True
+            print("low bat",self.battery_low,bat)
+            return 1
+        elif not bat:
+            self.battery_low = True
+        else:
+            self.battery_low = False
+        return 0
 
 try:
-
+    #while True:
     pixel_pin = board.D10
-    knob_input_pin = 14    
+    knob_input_pin = 14  
+    charge_pin = 15
     bouncetime = 100 #300
     
     gpf = filter_gp_0()
     
-    usbmt = usb_mount("/home/pi/usb_tests/usb_mount/",gpf,pixel_pin)
+    usbmt = usb_mount("/home/pi/usb_tests/usb_mount/",gpf,pixel_pin,charge_pin)
 
     usbmt.check_devices()
     usbmt.check_children()
@@ -389,12 +347,15 @@ try:
             flag0 = usbmt.check_devices()
             flag1 = usbmt.check_children()
             flag2 = usbmt.check_active_time()
-            if flag0==1 or flag1==1 or flag2==1:
+            flag3 = usbmt.check_battery()
+            #print(flag1)
+            if flag0==1 or flag1==1 or flag2==1 or flag3==1:
                 print("status changed \n")
                 usbmt.color_setter()
         time.sleep(zzzz)        
 
-except Exception as e:
+except Exception as e: # work on python 3.x
+    #logger.error('Error: '+ str(e))
     print("errorlog:")
     print( str(e) )
     nos = []
@@ -410,3 +371,4 @@ except Exception as e:
     logf.close()
     GPIO.cleanup()
     print("error")
+
